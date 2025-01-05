a95cfcaf8a4fbe32bf23193c6834e2d2eb76ad7d
DrAv0011 <43525295+drav0011@users.noreply.github.com>
2025-01-05 23:58:19 +0100

    feat(files): simplified ui
    - Simplified UI menus combining all "change setting" forms into a single ModalForm
    - restructured interfaces directory
    
    
    Former-commit-id: 3916e0f1bba8f08cc53d2f7bcf60f81dbaa82d96

--- addons/gameplay_changes/graves/packs/data/gametests/src/Events/ScriptEventReceive.ts
+++ addons/gameplay_changes/graves/packs/data/gametests/src/Events/ScriptEventReceive.ts
@@ -1,15 +1,19 @@
 import {
+	Player,
 	ScriptEventCommandMessageAfterEvent,
 	system
 } from '@minecraft/server';
 import { uninstall } from '../Actions';
 import { GravesScriptEvents } from '../Models';
 import { openConfigInterface } from '../UI';
+import { MinecraftEntityTypes } from '@minecraft/vanilla-data';
 
 system.afterEvents.scriptEventReceive.subscribe(({ id, sourceEntity }: ScriptEventCommandMessageAfterEvent): void => {
 	switch (id) {
 		case GravesScriptEvents.config:
-			openConfigInterface(sourceEntity);
+			if (sourceEntity?.matches({ type: MinecraftEntityTypes.Player })) {
+				openConfigInterface(sourceEntity as Player);
+			}
 			break;
 
 		case GravesScriptEvents.uninstall:
