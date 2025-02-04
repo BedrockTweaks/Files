4d40fcf0788839932d998549377c0bac5de9ae0a
SquatchHunter <paskz@hotmail.com>
2025-02-04 23:24:33 +1100

    feat(files): Anti Creeper Grief - rewrote & optimised - split bulk of code into Actions/explosion.ts - created Models/sounds.ts - created Models/particles.ts - update locally used .gitignore - created en_GB.lang

---
+++ addons/gameplay_changes/anti_creeper_grief/packs/data/gametests/src/Actions/settings.ts
@@ -0,0 +1,36 @@
+import { world } from '@minecraft/server';
+import { AntiCreeperGriefSettings, AntiCreeperGriefSettingsDynamicProperties } from '../Models';
+import { getProperties, setProperties } from '../Util';
+
+/**
+ * Initializes the general addon settings for AntiCreeperGrief if they are not already initialized.
+ * Sets default values for them and ensures required properties are set.
+ */
+export const initializeSettings = (): void => {
+	if (!getProperties<AntiCreeperGriefSettings>(world, AntiCreeperGriefSettingsDynamicProperties).initialized) {
+		setProperties(
+			world,
+			AntiCreeperGriefSettingsDynamicProperties,
+			{
+				initialized: true,
+				creepers_do_damage: false,
+			},
+		);
+	}
+};
+
+/**
+ * Retrieves the current addon settings from the world properties.
+ *
+ * @returns {AntiCreeperGriefSettings} - The current AntiCreeperGrief settings
+ */
+export const getSettings = (): AntiCreeperGriefSettings => getProperties<AntiCreeperGriefSettings>(world, AntiCreeperGriefSettingsDynamicProperties);
+
+/**
+ * Updates the addon settings in the world properties.
+ *
+ * @param {AntiCreeperGriefSettings} antiCreeperGriefSettings - The updated settings to be saved.
+ */
+export const setSettings = (antiCreeperGriefSettings: AntiCreeperGriefSettings): void => {
+	setProperties(world, AntiCreeperGriefSettingsDynamicProperties, antiCreeperGriefSettings);
+};
---
+++ addons/gameplay_changes/anti_creeper_grief/packs/data/gametests/src/Models/dynamicProperties.ts
@@ -0,0 +1,15 @@
+/**
+ * Definitions to use multiple Dynamic Properties as a Typed Object
+ * Enum keys and object properties should match for proper conversion.
+ */
+export enum AntiCreeperGriefSettingsDynamicProperties {
+	initialized = 'bt:acg.settings_initialized',
+	creepersDoDamage = 'bt:acg.creepers_do_damage'
+}
+
+export interface AntiCreeperGriefSettings {
+	// Indicates whether the AntiCreeperGrief system settings have been initialized.
+	initialized: boolean;
+	// Indicates whether creeper explosions will damage players or not.
+	creepersDoDamage: boolean;
+}
