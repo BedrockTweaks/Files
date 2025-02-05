65d86de8a156ec9654b9e59b6e5e537c2c916725
PAPISOP <85175982+PAPISOP@users.noreply.github.com>
2025-02-06 00:29:37 +0530

    update(files): Update the addon template to add more docs and update the graves addon
    1. Merged the both .gitignore files into one single .gitignore file at the top level
    2. Add more documentations to en_US and en_GB text files
    3. Improve the uninstall formatting by including the addon name and a new color scheme
    4. Updated Graves addon to reflect all the changes made in the addon template
    
    Co-authored-by: SquatchHunter <33571936+SquatchHunter@users.noreply.github.com>
    Co-authored-by: PAPISOP <85175982+PAPISOP@users.noreply.github.com>

--- addons/gameplay_changes/graves/packs/data/gametests/src/Events/GameRuleChange.ts
+++ addons/gameplay_changes/graves/packs/data/gametests/src/Events/GameRuleChange.ts
@@ -6,7 +6,7 @@ import {
 
 world.afterEvents.gameRuleChange.subscribe(({ rule, value }: GameRuleChangeAfterEvent): void => {
 	if (rule === GameRule.KeepInventory && value === false) {
-		world.sendMessage({ translate: 'bt.graves.keep_inventory' });
+		world.sendMessage({ translate: 'bt.graves.misc.keep_inventory' });
 
 		world.gameRules.keepInventory = true;
 	}
